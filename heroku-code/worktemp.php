dbd63104f195a4f9e646a09dfaa5f147058dca58
danagong <59593656+danagong@users.noreply.github.com>
2020-03-08 03:30:33 -0400

    Add files via upload

---
+++ heroku-code/worktemp.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Work</title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+
+<body>
+
+<?php include("includes/header.php"); ?>
+
+<main>
+ <div id = "thumbs">
+     <div class= "wrapper">
+         <a class ="project" href="cusd.php"></a>
+             <div>
+                 <div class= "project image">
+                     <div class = "intrinsic">
+                         <div class= "content-fill">
+                         <img class ="thumbnailimage" src="images/cusdthumb.png" alt= "CUSD"></img>
+                        </div>
+                    </div>
+                </div>
+                 </div>    
+            </div>
+     </div>
+
+ </main>
+</body>
+</html>
