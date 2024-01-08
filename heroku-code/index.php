1d8fadf765a07c671d77e5009ffaf005950ced15
dg588 <dg588@cornell.edu>
2024-01-08 02:28:59 -0500

    update

--- heroku-code/index.php
+++ heroku-code/index.php
@@ -17,7 +17,7 @@
     <div id= "about">
         <img class="profilepicture" src="images/selfie.png" alt= "Dana Gong" />
         <div id= "aboutme"> 
-            <p> <strong> Hello! </strong> My name is Dana Gong, a sophomore at Cornell University, studying Information Science with a double concentration in User Experience and Data Science.</p>
+            <p> <strong> Hello! </strong> My name is Dana Gong, a recent graduate of Cornell University, studying Information Science with a double concentration in User Experience and Data Science.</p>
             <p> I am currently coding and designing for Cornell University Sustainable Design, Cornell Rocketry Team, The Roper Center for Public Opinion Research, and Cornell Thrift</p>
             </p>
         </div>
